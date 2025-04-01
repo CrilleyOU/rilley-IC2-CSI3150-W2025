a0c8fa1d19c52c6d7440e2f5579c8c9034f0c448
CrilleyOU <crilleygcp@gmail.com>
2025-04-01 18:09:23 -0400

    cleaned up the styling and divs a bit more since I had some time

--- js/functionality.js
+++ js/functionality.js
@@ -4,3 +4,15 @@ const dog_button = document.getElementById("dog");
 const cow_button = document.getElementById("cow");
  
 
+// Add event listeners to the buttons
+cat_button.addEventListener("click", () => {
+    document.querySelector(".columns .hidden-box.cat").classList.remove("hidden");
+});
+
+dog_button.addEventListener("click", () => {
+    document.querySelector(".columns .hidden-box.dog").classList.remove("hidden");
+});
+
+cow_button.addEventListener("click", () => {
+    document.querySelector(".columns .hidden-box.cow").classList.remove("hidden");
+});
