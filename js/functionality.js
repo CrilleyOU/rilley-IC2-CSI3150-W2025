096f81bf205d5d0c58998d8aba3bd7efc877c43a
CrilleyOU <crilleygcp@gmail.com>
2025-04-01 11:16:30 -0400

    content on page, working on spacing

---
+++ js/functionality.js
@@ -0,0 +1,5 @@
+const picture_box = document.querySelectorAll(".columns .hidden-box");
+const cat_button = document.getElementById("cat");
+const dog_button = document.getElementById("dog");
+const cow_button = document.getElementById("cow");
+ 
